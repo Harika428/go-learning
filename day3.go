5bf1b626d40bd2edba7d0747884127ebb0dd85b0
Harika428 <103199014+Harika428@users.noreply.github.com>
2022-04-12 22:31:40 +0530

    day3 exercise

---
+++ day3.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+//assignening value to a variable,using var keyword//
+var x int = 7
+var y string = "hello world"
+//we can use ":" instead of using var type//
+x1 := "puppy"
+//finding the length of a string//
+fmt.Println(len("hello world"))
+fmt.Println(x, y)
+fmt.Println("dog's name is", x1)	 
+      }
